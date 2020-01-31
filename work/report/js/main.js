55cf1933af606b179c2bbe397379fbdbb9938c9f
Amir Salaheldin Gissel <amir.gissel0016@stud.hkr.se>
2020-01-31 22:16:46 +0100

    finished the school part

--- work/report/js/main.js
+++ work/report/js/main.js
@@ -1,10 +1,44 @@
 /**
  * A function to wrap it all in.
  */
-(function () {
-    "use strict";
 
 
 
-    console.log("All ready.");
-}());
+function selectedData() {
+    var selectedItem = document.getElementById("municipalities").value;
+    
+    fetch('data/'+selectedItem+'.json')
+    .then((response) => {
+        return response.json();
+    })
+    .then((myJson) => {
+        console.log(myJson.Skolenheter);
+
+        if(myJson.Skolenheter.length>0){
+            var temp ="";
+            myJson.Skolenheter.forEach((u) => {
+                temp+="<tr>";
+                temp += "<td>"+u.Kommunkod+"</td>"
+                temp += "<td>"+u.PeOrgNr+"</td>"
+                temp += "<td>"+u.Skolenhetskod+"</td>"
+                temp += "<td>"+u.Skolenhetsnamn+"</td></tr>";
+
+            });
+    
+        document.getElementById('data').innerHTML = temp;
+    }
+            });
+        
+}
+function moveTheDuck() {
+    var left = "style='float: right;'";
+    var right = "style='float: left;'";
+    var duckPos = document.getElementById("duck").style.cssFloat;
+
+    if(duckPos == "left"){
+        document.getElementById("duck").style.cssFloat = "right";
+    }else {
+        document.getElementById("duck").style.cssFloat = "left";
+    }
+        
+}
